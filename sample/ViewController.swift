d576c718ab552f220ab3b19e602ff93a801dc41b
pedrofsn <pedrokra@gmail.com>
2018-02-24 10:22:51 -0300

    first commit

--- sample/ViewController.swift
+++ sample/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onSegmentedControlChange(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.view.backgroundColor = UIColor.red
+        case 1:
+            self.view.backgroundColor = UIColor.green
+        case 2:
+            self.view.backgroundColor = UIColor.purple
+        case 3:
+            self.view.backgroundColor = UIColor.yellow
+        case 4:
+            self.view.backgroundColor = UIColor.white
+        default:
+            self.view.backgroundColor = UIColor.black
+        }
+    }
+    
+    @IBAction func onSwitchValueChanged(_ uiSwitch: UISwitch) {
+        changeVisibility(uiSwitch.isOn)
+    }
+    
+    func changeVisibility(_ visible : Bool){
+        imageView?.isHidden = !visible
+        segmentedControl?.isHidden = !visible
+        label?.isHidden = !visible
+    }
 }
-
